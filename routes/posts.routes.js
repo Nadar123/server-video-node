ae9257f4f1d88b3095a789ebdbf11a9510e66f28
nadar123 <nadar.rosano@gmail.com>
2020-08-05 00:00:28 +0300

    post route auth

--- routes/posts.routes.js
+++ routes/posts.routes.js
@@ -3,9 +3,9 @@ const router = express.Router();
 
 // models
 const Post = require('../models/post');
-const checkAuth = require('../middleware/check');
+// const checkAuth = require('../middleware/check');
 
-router.post('/',checkAuth, async (req, res, next) => {
+router.post('/', async (req, res, next) => {
   try {
     const post = await new Post({
       title: req.body.title,
@@ -50,22 +50,12 @@ router.get("/:id", (req, res, next) => {
   });
 });
 
-router.delete('/:id', checkAuth, async(req,res, next) => {
+router.delete('/:id', async(req,res, next) => {
   Post.deleteOne({_id:req.params.id})
   .then(result => {
     console.log(result);
     res.status(200).json({message: "Post Deleted"});
   });
-  // try {
-  //   const post = await Post.deleteOne({_id: req.params.id})
-  //   console.log(result);
-  //   res.status(200).json({message: 'Post Deleted', 
-  //   result:post
-  // });
-    
-  // } catch (error) {
-    
-  // }
 });
 
 module.exports = router;
